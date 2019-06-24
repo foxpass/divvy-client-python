1b2844587250858e57d0374021798343f51ef8cd
kumar35959 <kumar@foxpass.com>
2019-06-24 11:47:05 -0700

    Updated regex as per the new requirements

--- divvy/protocol.py
+++ divvy/protocol.py
@@ -19,7 +19,7 @@ Response = namedtuple(
 
 
 class Translator(object):
-    STRING_REGEXP = re.compile('^[^"\\s]+$')
+    STRING_REGEXP = re.compile(r'^[^"\n]+$')
     RESPONSE_REGEXP = re.compile('^OK (true|false) (-?\\d+) (-?\\d+)$')
     ERROR_REGEXP = re.compile('^ERR (unknown|unknown-command) "?([^"]+)"?$')
 
